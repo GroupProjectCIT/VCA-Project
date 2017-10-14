9c288e4fb27e6a2fdcdcc31accfb078dc3c6cf92
Adrian Poznan <31847641+P0Z1@users.noreply.github.com>
2017-10-14 20:48:32 +0100

    Login functionality
    Changed and added js code for logging in,
    
    changed logout link to a button to work with js,
    
    can log in but have problems with navigation:
    for some reason on successful login when going to home page, the page reloads itself constantly and there are no loops anywhere in the code. will keep work on it.

--- WebpageV.1.1/VCA-JavaScript.js
+++ WebpageV.1.1/VCA-JavaScript.js
@@ -1,8 +1,84 @@
-
-
-
-function login()
+(function()
 {
-	location.href="VCA-Home.html";
-}
 
+	//code from firebase
+	// Initialize Firebase
+	const config =
+	{
+		apiKey: "AIzaSyBXBZz8ADFzd1AG-SrBcFh2ZSkdZAS7MRw",
+		authDomain: "virtual-care-assistant-462a4.firebaseapp.com",
+		databaseURL: "https://virtual-care-assistant-462a4.firebaseio.com",
+		storageBucket: "virtual-care-assistant-462a4.appspot.com",
+	};
+	
+	firebase.initializeApp(config);
+	
+	
+	//get elements for log in
+	const emailIn = document.getElementById('emailInput');
+	const passwordIn = document.getElementById('passwordInput');
+	const logInBtn = document.getElementById('logInBtn');
+	const logOutBtn = document.getElementById('logOutBtn');
+	
+	
+	//add listener to login button
+	logInBtn.addEventListener('click', e=>
+	{
+		//pull email and password from elements
+		const email= emailIn.value;
+		const password = passwordIn.value;
+		const auth = firebase.auth();
+		
+		//attempt to log into firebase with email and pass
+		const promise = auth.signInWithEmailAndPassword(email,password);
+		
+		//catch errors and log to console
+		promise.catch(e => console.log(e.message));
+		
+	});
+	
+	//add listener to logOutBtn
+	logOutBtn.addEventListener('click', e=>
+	{
+		//sign out the current user
+		firebase.auth().signOut();
+		
+		//go to login page
+		window.location.href = "VCA-Login.html"
+			
+	});
+	
+	
+	//realtime authorization listener
+	firebase.auth().onAuthStateChanged(firebaseUser =>
+	{
+		//checks waht user is loged in
+		var user = firebase.auth().currentUser;
+		
+		if (user) 
+		{
+			// User is signed in go to home page
+			window.location.href = "VCA-Home.html";
+		}
+		else
+		{
+			// No user is signed in do nothing
+		}
+		
+		//log info on login to console
+		if(firebaseUser)
+		{
+			//logs to console user info
+			console.log(firebaseUser);
+		}
+		else
+		{
+			//logs to console a message
+			console.log("user logedOut/not logged in...thx obama!");
+		}
+	});
+	
+	
+
+	
+}());
