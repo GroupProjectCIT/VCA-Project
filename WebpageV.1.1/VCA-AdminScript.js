88edd0a4771bd74cba71c424650845ebc3f2a5a0
Adrian Poznan <31847641+P0Z1@users.noreply.github.com>
2017-10-18 18:27:35 +0100

    Admin functionality progress
    some of the front end elements on the admin add user page

---
+++ WebpageV.1.1/VCA-AdminScript.js
@@ -0,0 +1,21 @@
+(function()
+{
+	//get add button for the admin
+	const adminAddUserBtn = document.getElementById('adminAddUserBtn');
+
+
+	if()
+	{
+		//get the value of selected radio button
+		const accTypeFromRadio = document.getElementById("patientRadioInput").value;
+		
+		alert(accTypeFromRadio);
+		//disable password Text field
+		document.getElementById("passwordTxtField").disable = "true";
+		document.getElementById("").disable = "true";
+	}
+
+
+	
+
+});
