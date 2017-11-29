5c3027de913a942168916141b341b187655f1df8
Shane Marshall <s.marshall@mycit.ie>
2017-11-29 14:37:05 +0000

    Weather
    Added weather and playback of weather

--- CareAssistant/app/src/main/java/com/example/android/careassistant/WeatherActivity.java
+++ CareAssistant/app/src/main/java/com/example/android/careassistant/WeatherActivity.java
@@ -6,6 +6,248 @@ import android.support.v7.app.AppCompatActivity;
  * Created by Shane on 27/11/2017.
  */
 
-public class WeatherActivity extends AppCompatActivity {
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.support.v4.app.ActivityCompat;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.squareup.picasso.Picasso;
+import java.lang.reflect.Type;
+import com.example.android.careassistant.Common.Common;
+import com.example.android.careassistant.Helper.Helper;
+import com.example.android.careassistant.Model.OpenWeatherMap;
+import android.speech.tts.TextToSpeech;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class WeatherActivity extends AppCompatActivity implements LocationListener {
+
+    TextView txtCity, txtDescription, txtCelsius;
+    ImageView imageView;
+
+    LocationManager locationManager;
+    String provider;
+    static double lat, lng;
+    OpenWeatherMap openWeatherMap = new OpenWeatherMap();
+
+    int MY_PERMISSION = 0;
+
+    //Text to Speech
+    TextToSpeech toSpeech;
+    int result;
+    String text;
+    String city;
+    String country;
+    String description;
+    double temp;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+
+
+
+        //Text to Speech
+        toSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status==TextToSpeech.SUCCESS) {
+                    result = toSpeech.setLanguage(Locale.US);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Feature is not supported", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        //Control
+        txtCity = findViewById(R.id.txtCity);
+
+        txtDescription =  findViewById(R.id.txtDescription);
+
+        txtCelsius = findViewById(R.id.txtCelsius);
+        imageView = findViewById(R.id.imageView);
+
+
+        //Get Coordinates
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        provider = locationManager.getBestProvider(new Criteria(), false);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+
+            ActivityCompat.requestPermissions(WeatherActivity.this, new String[]{
+                    Manifest.permission.INTERNET,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_NETWORK_STATE,
+                    Manifest.permission.SYSTEM_ALERT_WINDOW,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+
+
+            }, MY_PERMISSION);
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+        if (location == null)
+            Log.e("TAG","No Location");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(WeatherActivity.this, new String[]{
+                    Manifest.permission.INTERNET,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_NETWORK_STATE,
+                    Manifest.permission.SYSTEM_ALERT_WINDOW,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+
+
+            }, MY_PERMISSION);
+        }
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(WeatherActivity.this, new String[]{
+                    Manifest.permission.INTERNET,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_NETWORK_STATE,
+                    Manifest.permission.SYSTEM_ALERT_WINDOW,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+
+
+            }, MY_PERMISSION);
+        }
+        locationManager.requestLocationUpdates(provider, 400, 1, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        lat = location.getLatitude();
+        lng = location.getLongitude();
+
+
+        new GetWeather().execute(Common.apiRequest(String.valueOf(lat),String.valueOf(lng)));
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    private class GetWeather extends AsyncTask<String,Void,String>{
+        ProgressDialog pd = new ProgressDialog(WeatherActivity.this);
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pd.setTitle("Please wait...");
+            pd.show();
+
+        }
+
+
+        @Override
+        protected String doInBackground(String... params) {
+            String stream = null;
+            String urlString = params[0];
+
+            Helper http = new Helper();
+            stream = http.getHTTPData(urlString);
+            return stream;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if(s.contains("Error: Not found city")){
+                pd.dismiss();
+                return;
+            }
+            Gson gson = new Gson();
+            Type mType = new TypeToken<OpenWeatherMap>(){}.getType();
+            openWeatherMap = gson.fromJson(s,mType);
+            pd.dismiss();
+
+            city = openWeatherMap.getName();
+            country = openWeatherMap.getSys().getCountry();
+            description = openWeatherMap.getWeather().get(0).getDescription();
+            temp = openWeatherMap.getMain().getTemp();
+
+            txtCity.setText(String.format("%s,%s",city,country));
+            txtDescription.setText(String.format("%s",description));
+            txtCelsius.setText(String.format("%.2f °C",temp));
+            Picasso.with(WeatherActivity.this)
+                    .load(Common.getImage(openWeatherMap.getWeather().get(0).getIcon()))
+                    .into(imageView);
+
+        }
+
+    }
+
+
+    //Text to Speech Weather
+    public void speakWeather(View view) {
+        switch (view.getId()) {
+            case R.id.playWeather:
+                if(result==TextToSpeech.LANG_MISSING_DATA || result==TextToSpeech.LANG_NOT_SUPPORTED) {
+                    Toast.makeText(getApplicationContext(), "Feature is not supported", Toast.LENGTH_SHORT).show();
+                } else {
+                    text = "It is " + temp + "degrees celsius outside, with " + description ;
+                    toSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        if(toSpeech!=null) {
+            toSpeech.stop();
+            toSpeech.shutdown();
+        }
+    }
+
+
+
+
+
 
 }
